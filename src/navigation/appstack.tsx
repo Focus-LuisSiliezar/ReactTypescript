80222e8a47954ba3456cc7318d73c35a0d4f751e
Focus-LuisSiliezar <luis.siliezar@focusservices.com>
2022-11-08 16:49:17 -0600

    editing stack options

--- src/navigation/appstack.tsx
+++ src/navigation/appstack.tsx
@@ -5,8 +5,18 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 const { Navigator, Screen } = createNativeStackNavigator();
 const AuthStack: FC = () => {
     return (
-        <Navigator>
-            <Screen name="home" component={Home} />
+        <Navigator
+        screenOptions={{
+            headerStyle: { backgroundColor: 'white' },
+            headerTitleAlign: 'center',
+            headerShadowVisible: false,
+            contentStyle: {
+              backgroundColor: 'white',
+            }
+          }}
+        
+        >
+            <Screen name="home" component={Home} options={{title: 'HOME'}} />
             <Screen name="dashboard" component={Dashboard} />
         </Navigator>
     );
