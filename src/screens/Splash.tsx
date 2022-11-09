91a5774c8bddc00227b4b93262e6508f926c6123
Focus-LuisSiliezar <luis.siliezar@focusservices.com>
2022-11-09 15:24:18 -0600

    SplashScreen Working

--- src/screens/Splash.tsx
+++ src/screens/Splash.tsx
@@ -1,15 +1,40 @@
 import React from 'react';
 import LottieView from 'lottie-react-native'; // if you have "esModuleInterop": true
+import { StackActions, useFocusEffect, useNavigation } from '@react-navigation/native';
 // import LottieView = require('lottie-react-native'); // otherwise you have "esModuleInterop": false
 
-export default class Splash extends React.PureComponent {
-  render() {
-    return (
-      <LottieView
-        source={require('../../assets/lottie-splash.json')}
-        autoPlay
-        loop
-      />
+
+const Splash: React.FC = () => {
+  const navigation = useNavigation();
+  function SplashScreenDelay() {
+    navigation.dispatch(
+      StackActions.replace('Home')
     );
+    console.log('Time is up');
   }
+  setTimeout(() => {
+    SplashScreenDelay();
+  }, 4400);
+
+  return (
+    <LottieView
+      source={require('../../assets/lottie-splash.json')}
+      autoPlay
+      loop
+    />
+  );
 }
+
+export default Splash;
+// export default class Splash extends React.PureComponent {
+
+//   render() {
+//     return (
+//       <LottieView
+//         source={require('../../assets/lottie-splash.json')}
+//         autoPlay
+//         loop
+//       />
+//     );
+//   }
+// }
