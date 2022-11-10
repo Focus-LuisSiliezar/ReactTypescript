39f8970a8cfd7f12faee5244bebd158b425cddcd
Focus-LuisSiliezar <luis.siliezar@focusservices.com>
2022-11-10 08:11:04 -0600

    moviePosters

--- src/lists/MovieList.tsx
+++ src/lists/MovieList.tsx
@@ -4,63 +4,63 @@ import { MovieItem } from '../components';
 const MOVIES = [
     {
         'id': '1',
-        'movieLink': 'Link'
+        'movieLink': 'https://www.washingtonpost.com/graphics/2019/entertainment/oscar-nominees-movie-poster-design/img/black-panther-web.jpg'
     },
     {
         'id': '2',
-        'movieLink': 'Link'
+        'movieLink': 'https://i0.wp.com/batman-news.com/wp-content/uploads/2022/01/The-Batman-2022-Movie-Poster-01.jpeg?fit=2764%2C4096&quality=80&strip=info&ssl=1'
     },
     {
         'id': '3',
-        'movieLink': 'Link'
+        'movieLink': 'https://images.thedirect.com/media/photos/posd1_1.jpg'
     },
     {
         'id': '4',
-        'movieLink': 'Link'
+        'movieLink': 'https://www.reeldeals.com/Images/HomeImages/37705.jpg'
     },
     {
         'id': '5',
-        'movieLink': 'Link'
+    'movieLink': 'https://i.ebayimg.com/images/g/3S8AAOSwAQpfjVRI/s-l500.jpg'
     },
     {
         'id': '6',
-        'movieLink': 'Link'
+        'movieLink': 'https://cdn.shopify.com/s/files/1/0608/8264/9321/files/blade_runner_2049_17_u.jpg?v=1637179276'
     },
     {
         'id': '7',
-        'movieLink': 'Link'
+        'movieLink': 'https://cdn.shopify.com/s/files/1/0057/3728/3618/products/5cae019e64c0ee10ead36a00e60f0137_8b8714b2-b733-44ec-8f3f-f131a04915b5_240x360_crop_center.progressive.jpg?v=1614780406'
     },
     {
         'id': '8',
-        'movieLink': 'Link'
+        'movieLink': 'https://cdn.shopify.com/s/files/1/0037/8008/3782/products/IMG_7259_1024x1024@2x.jpg?v=1640349274'
     },
     {
         'id': '9',
-        'movieLink': 'Link'
+        'movieLink': 'https://i.etsystatic.com/18242346/r/il/54bef3/2802148755/il_570xN.2802148755_7wzt.jpg'
     },
     {
         'id': '10',
-        'movieLink': 'Link'
+        'movieLink': 'https://creativereview.imgix.net/content/uploads/2018/12/Unknown-5.jpeg?auto=compress,format&q=60&w=2024&h=3000'
     },
 ];
 const renderItem = ({ item }: any) => (
-    <MovieItem onPress={() => { console.log(item) }} />
+    <MovieItem uri={item.movieLink} onPress={() => { console.log(item.id); }} />
 );
 
 
 const MovieList: React.FC = () => {
     return (
-            <FlatList
-                numColumns={2}
-                contentContainerStyle={{ paddingBottom: 20 }}
-                ItemSeparatorComponent={() => <View style={{ height: 12 }} />}
-                showsVerticalScrollIndicator={false}
-                showsHorizontalScrollIndicator={false}
-                style={{ flex: 1 }}
-                data={MOVIES}
-                renderItem={renderItem}
-                keyExtractor={item => item.id}
-            />
+        <FlatList
+            numColumns={2}
+            contentContainerStyle={{ paddingBottom: 20 }}
+            ItemSeparatorComponent={() => <View style={{ height: 12 }} />}
+            showsVerticalScrollIndicator={false}
+            showsHorizontalScrollIndicator={false}
+            style={{ flex: 1 }}
+            data={MOVIES}
+            renderItem={renderItem}
+            keyExtractor={item => item.id}
+        />
 
 
     );
