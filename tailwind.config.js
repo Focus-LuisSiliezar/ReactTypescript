8dadc9c628ea023352fffb766b4eaf0d087e914a
Focus-LuisSiliezar <luis.siliezar@focusservices.com>
2022-11-17 08:34:09 -0600

    initial tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}'", 
+    // "./src/screens/WatchLater.{js,jsx,ts,tsx}'", 
+    "./App.{js,jsx,ts,tsx}", 
+    // "./screens/",
+    // "./lists/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
